0e41d1cc571c43905251007e167967d561bde2d0
Diego Barcellos <diegocb81@gmail.com>
2020-11-01 20:14:12 -0300

    Atualizando o mapa com javascript

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,15 @@
-window.onload = function () {
-    
+window.onload = function(){
+
+    var map;
+
+    function initialize(){
+        var mapProp = {
+            center: new google.maps.LatLng(-27.436274,-48.405307),
+            scrollWheel: false,
+            zoom: 10,
+            MapTypeId:google.maps.MapTypeId.ROADMAP
+        }
+        map = new google.maps.Map(document.getElementById("mapa"),mapProp);
+    }
+    initialize();
 }
