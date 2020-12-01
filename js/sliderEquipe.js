a322662621d39580f6cdf445e43981e0a5018f1d
Diego Barcellos <diegocb81@gmail.com>
2020-12-01 18:41:29 -0300

    Atualizando o projeto

--- js/sliderEquipe.js
+++ js/sliderEquipe.js
@@ -1,3 +1,20 @@
 $(function () {
-    alert('Olá mundo');
+    
+    initSlider();
+
+    function initSlider() {
+        var amt = $('.sobre-autor').length;
+        var sizeContainer = 100 * amt;
+        var sizeBoxSingle = 100 / amt;
+        $('.sobre-autor').css('width',sizeBoxSingle+'%');
+        $('.scroll-wraper').css('width',sizeContainer+'%');
+
+
+        for(var i = 0; i < amt; i++){
+            if(i == 0)
+                $('.slider-bullets').append('<span style="background-color:rgb(170,170,170);"></span>');
+            else
+                $('.slider-bullets').append('<span></span>');
+        }
+    }
 })
