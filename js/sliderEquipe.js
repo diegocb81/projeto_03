9fa393bfa497015d7f8d578f56b88ba181878ef5
Diego Barcellos <diegocb81@gmail.com>
2020-12-02 09:49:09 -0300

    Criando slider

--- js/sliderEquipe.js
+++ js/sliderEquipe.js
@@ -1,9 +1,14 @@
 $(function () {
+
+    var delay = 3000;
+    var curIndex = 0;
+    var amt;
     
     initSlider();
+    autoPlay();
 
     function initSlider() {
-        var amt = $('.sobre-autor').length;
+        amt = $('.sobre-autor').length;
         var sizeContainer = 100 * amt;
         var sizeBoxSingle = 100 / amt;
         $('.sobre-autor').css('width',sizeBoxSingle+'%');
@@ -17,4 +22,24 @@ $(function () {
                 $('.slider-bullets').append('<span></span>');
         }
     }
+
+    function autoPlay() {
+        setInterval(function(){
+                curIndex++;
+                if(curIndex == amt)
+                    curIndex = 0;
+                goToSlider(curIndex);
+        },delay)
+    }
+
+    function goToSlider(curIndex){
+            var offSetX = $('.sobre-autor').eq(curIndex).offset().left - $('.scroll-wraper').offset().left;
+            $('.slider-bullets span').css('background-color','rgb(200,200,200)');
+            $('.slider-bullets span').eq(curIndex).css('background-color','#3686be');
+            $('.scrollEquipe').stop().animate({'scrollLeft': offSetX+'px'});
+    }
+
+    $(window).resize(function(){
+        $('.scrollEquipe').stop().animate({'scrollLeft': 0});
+    })
 })
